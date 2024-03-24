a3ce0af8d43303ae4b3f24fc5e5c46bdd4de62cf
frealem <frealemtekalign156@gmail.com>
2024-03-24 22:25:18 +0300

    intg2

--- clientside/src/pages/authPage.js
+++ clientside/src/pages/authPage.js
@@ -1,4 +1,4 @@
-import React, { useRef,useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import {
   Button,
   Checkbox,
@@ -12,59 +12,71 @@ import {
   useTheme,
 } from "@mui/material";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import {Field, Form, Formik} from 'formik';
-import * as yup from 'yup';
-import {useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import Dropzone from 'react-dropzone';
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
+import { Field, Form, Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import Dropzone from "react-dropzone";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import PhoneInputField from "../components/phoneInput";
-
+import { setLogin } from "../components/States/authSlice";
+import { useLoginMutation } from "../components/States/authApiSlice";
 
 //client side validation
 
-const registerSchema=yup.object().shape({
-  fullName:yup.string().required("required"),
-  phone:yup.string().required("required"),
-  password:yup.string().required("required"),
-  email:yup.string().email("invalid email").required("required"),
-  address:yup.string().required("required")
+const registerSchema = yup.object().shape({
+  fullName: yup.string().required("required"),
+  phone: yup.string().required("required"),
+  password: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  address: yup.string().required("required"),
 });
 
-const loginSchema=yup.object().shape({
-  email:yup.string().email("invalid email").required("required"),
-  password:yup.string().required("required"),
-})
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
 
-const initialValuesLogin={
-  email:"",
-password:"",
-}
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
 
-const initialValuesSignup={
-  email:"",
-  phone:"",
-  address:"",
-  fullName:"",
-  password:"",
-}
+const initialValuesSignup = {
+  email: "",
+  phone: "",
+  address: "",
+  fullName: "",
+  password: "",
+};
 
 const AuthPage = () => {
   const theme = useTheme();
   const [open, setOpen] = useState(true);
   const [signUp, setSignUp] = useState(false);
-const userRef=useRef();
-const navigate=useNavigate();
-  //handle submit
-const handleSubmitLogin=()=>{
-  console.log('user logged in');
-}
+  const userRef = useRef();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
 
-const handleSubmitSignup=()=>{
-  console.log('user signed up');
-}
- 
+
+  useEffect(()=>{
+    useRef.current.focus()
+  },[])
+
+  useEffect(()=>{
+// for errror
+
+  },[]) //user,password
+  //handle submit
+  const handleSubmitLogin = () => {
+    console.log("user logged in");
+  };
+
+  const handleSubmitSignup = () => {
+    console.log("user signed up");
+  };
 
   const openDialog = () => {
     setOpen(true);
@@ -110,49 +122,49 @@ const handleSubmitSignup=()=>{
             </IconButton>
           </DialogTitle>
           <DialogContent>
-          <Formik
+            <Formik
               initialValues={initialValuesLogin}
               onSubmit={handleSubmitLogin}
               validationSchema={loginSchema}
             >
-            {({
-              values,
-              errors,
-              touched,
-              handleBlur,
-              handChange,
-              handleSubmit,
-              setFieldValue,
-              resetForm,
-            })=>(<Form>
-                <Stack spacing={2} margin={2}>
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    label="Email"
-                    name="email"
-                  />
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                  <Button
-                    type="submit"
-                    backgroundColor={theme.palette.secondary.light}
-                    variant="contained"
-                  >
-                    Sign In
-                  </Button>
-                  <a onClick={handleSignUpClick}>
-                    I don't have an account, Sign Up
-                  </a>
-                </Stack>
-              </Form>)
-            }
-              
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+              }) => (
+                <Form>
+                  <Stack spacing={2} margin={2}>
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      label="Email"
+                      name="email"
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      label="Password"
+                      name="password"
+                      type="password"
+                    />
+                    <Button
+                      type="submit"
+                      backgroundColor={theme.palette.secondary.light}
+                      variant="contained"
+                    >
+                      Sign In
+                    </Button>
+                    <a onClick={handleSignUpClick}>
+                      I don't have an account, Sign Up
+                    </a>
+                  </Stack>
+                </Form>
+              )}
             </Formik>
           </DialogContent>
         </Dialog>
@@ -183,79 +195,87 @@ const handleSubmitSignup=()=>{
             </IconButton>
           </DialogTitle>
           <DialogContent>
-          <Formik 
-            initialValues={initialValuesSignup}
+            <Formik
+              initialValues={initialValuesSignup}
               onSubmit={handleSubmitSignup}
-              validationSchema={registerSchema}>
-                {({
-                  values,
-              errors,
-              touched,
-              handleBlur,
-              handChange,
-              handleSubmit,
-              setFieldValue,
-              resetForm,
-                })=>(<Form>
-                <Stack spacing={2} margin={2}>
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    name="fullName"
-                    placeholder="Enter your full name"
-                  />
-              <Field 
-              as={TextField}
-              variant="outlined"
-              name="phone"
-               component={PhoneInputField}
-               inputStyle={{ width: '100%',outerHeight:"100%" }} // Set the width of the phone input field
-      containerStyle={{ width: '100%',outerHeight:"100%"  }} 
-       />
-            <Field
-                    as={TextField}
-                    variant="outlined"
-                    name="address"
-                    placeholder="Enter your address"
-                  />
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    name="email"
-                    placeholder="Enter your email"
-                  />
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    name="password"
-                    type="password"
-                    placeholder="Enter new password"
-                  />
-                  <Field
-                    as={TextField}
-                    variant="outlined"
-                    name="confirmPassword"
-                    type="password"
-                    placeholder="confirm your password"
-                  />
-                  <FormControlLabel
-                    control={<Field as={Checkbox} color="primary" name="acceptAgreement" />}
-                    label="Accept All Agreement And Submit"
-                  />
-                  <Button
-                    type="submit"
-                    backgroundColor={theme.palette.secondary.light}
-                    variant="contained"
-                  >
-                    Sign Up
-                  </Button>
-                  <a onClick={handleSignInClick}>
-                    I already have an account, Sign In
-                  </a>
-                </Stack>
-              </Form>)}
-              </Formik>
-            
+              validationSchema={registerSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+              }) => (
+                <Form>
+                  <Stack spacing={2} margin={2}>
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="fullName"
+                      placeholder="Enter your full name"
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="phone"
+                      component={PhoneInputField}
+                      inputStyle={{ width: "100%", outerHeight: "100%" }} // Set the width of the phone input field
+                      containerStyle={{ width: "100%", outerHeight: "100%" }}
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="address"
+                      placeholder="Enter your address"
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="email"
+                      placeholder="Enter your email"
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="password"
+                      type="password"
+                      placeholder="Enter new password"
+                    />
+                    <Field
+                      as={TextField}
+                      variant="outlined"
+                      name="confirmPassword"
+                      type="password"
+                      placeholder="confirm your password"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Field
+                          as={Checkbox}
+                          color="primary"
+                          name="acceptAgreement"
+                        />
+                      }
+                      label="Accept All Agreement And Submit"
+                    />
+                    <Button
+                      type="submit"
+                      backgroundColor={theme.palette.secondary.light}
+                      variant="contained"
+                    >
+                      Sign Up
+                    </Button>
+                    <a onClick={handleSignInClick}>
+                      I already have an account, Sign In
+                    </a>
+                  </Stack>
+                </Form>
+              )}
+            </Formik>
           </DialogContent>
         </Dialog>
       )}
